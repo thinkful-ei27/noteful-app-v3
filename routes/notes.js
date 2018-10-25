2621fede7da8080858f6a246a94fbc88035e838e
Chris Klanac <cklanac@users.noreply.github.com>
2018-10-25 12:32:45 -0400

    fix: change from toObject to toJSON

--- routes/notes.js
+++ routes/notes.js
@@ -31,28 +31,7 @@ router.get('/', (req, res, next) => {
     });
 });
 
-/* ========== GET/READ A SINGLE ITEM ========== */
-router.get('/:id', (req, res, next) => {
-  const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    const err = new Error('The `id` is not valid');
-    err.status = 400;
-    return next(err);
-  }
-
-  Note.findById(id)
-    .then(result => {
-      if (result) {
-        res.json(result);
-      } else {
-        next();
-      }
-    })
-    .catch(err => {
-      next(err);
-    });
-});
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
